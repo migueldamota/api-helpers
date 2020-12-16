5288ee0b6336f6f018a8497b7f09517ac77c9ee2
Miguel da Mota <miguel@damota.de>
2020-12-16 22:17:26 +0100

    :bug: added gitignore

---
+++ README.md
@@ -0,0 +1,10 @@
+## Express Error Codes for APIs
+___
+
+### How to use it
+Install package with ``npm i @migueldamota/api-helpers --save``
+
+### Usage:
+Status Codes (error_status): https://httpstatuses.com
+
+``error(error_status, code)``
---
+++ index.js
@@ -0,0 +1,24 @@
+const errors = {
+    200: "200: OK",
+    201: "201: Not Authorized",
+    204: "204: No Content",
+
+    304: "304: Not Modified",
+
+    400: "400: Bad Request",
+    401: "401: Not Authorized",
+    403: "403: Forbidden",
+    404: "404: Not Found",
+    405: "405: Method Not Allowed",
+
+    500: "500: Internal Server Error",
+}
+
+const error = (error, code = 0) => {
+    return {
+        message: errors[error],
+        code,
+    }
+}
+
+module.exports = error;
